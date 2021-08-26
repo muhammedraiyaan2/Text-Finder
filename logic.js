722bf258194f07bc7dc179d0affe19c64144ce16
Muhammad Raiyaan <83336994+muhammedraiyaan2@users.noreply.github.com>
2021-08-26 16:41:10 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -10,6 +10,7 @@ nav.style.background="#f8f9fa"
 nav.style.color="#212529"
 btn.addEventListener("click", function(){
     output.innerText=`${para.value.split(vari.value).length-1} words found
+    ${para.value.length} characters
     index number ${para.value.indexOf(vari.value)+1} to ${para.value.indexOf(vari.value)+1+vari.value.length}`
 })
 // darkmode
